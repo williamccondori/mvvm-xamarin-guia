604394843e794d63c3d580ca3565ecc5b7c08bfe
William Condori Quispe <williamccodori@outlook.com>
2022-01-31 00:01:28 -0500

    Label binding

--- MvvmGuia/VistaModelo/VMPagina1.cs
+++ MvvmGuia/VistaModelo/VMPagina1.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -7,7 +7,9 @@ namespace MvvmGuia.VistaModelo
     public class VMPagina1 : BaseViewModel
     {
         #region VARIABLES
-        private string _Mensaje;
+        private string _N1;
+        private string _N2;
+        private string _Respuesta;
         #endregion
         #region CONSTRUCTOR
         public VMPagina1(INavigation navigation)
@@ -16,26 +18,34 @@ namespace MvvmGuia.VistaModelo
         }
         #endregion
         #region OBJETOS
-        public string Mensaje
+        public string N1
         {
-            get => _Mensaje;
-            set => SetValue(ref _Mensaje, value);
+            get => _N1;
+            set => SetValue(ref _N1, value);
+        }
+
+        public string N2
+        {
+            get => _N2;
+            set => SetValue(ref _N2, value);
+        }
+        public string Respuesta
+        {
+            get => _Respuesta;
+            set => SetValue(ref _Respuesta, value);
         }
         #endregion
         #region PROCESOS
-        public async Task AlertaAsync()
+        public void Sumar()
         {
-            await DisplayAlert("Titulo", Mensaje, "OK");
-        }
-
-        public void Alerta()
-        {
-
+            double n1 = Convert.ToDouble(N1);
+            double n2 = Convert.ToDouble(N2);
+            double respuesta = n1 + n2;
+            Respuesta = respuesta.ToString();
         }
         #endregion
-        #region COMANDO
-        public ICommand AlertaAsyncCommand => new Command(async () => await AlertaAsync());
-        public ICommand AlertaCommand => new Command(() => Alerta());
+        #region COMANDO 
+        public ICommand SumarCommand => new Command(() => Sumar());
         #endregion
     }
 }
