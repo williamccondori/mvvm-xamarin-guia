ed2348b8034abe79ce6ecac47747402975a43afc
William Condori Quispe <williamccodori@outlook.com>
2022-01-30 21:33:18 -0500

    se agrega patron mvvm

---
+++ MvvmGuia/VistaModelo/BaseViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MvvmGuia.VistaModelo
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public INavigation Navigation;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task DisplayAlert(string title, string message, string cancel)
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, cancel);
+        }
+
+        public async Task<bool> DisplayAlert(string title, string message, string accept, string cancel)
+        {
+            return await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                return;
+            }
+
+            backingField = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private ImageSource _image;
+        public ImageSource Image
+        {
+            get { return _image; }
+            set => SetProperty(ref _image, value);
+        }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set => SetProperty(ref _title, value);
+        }
+
+        private bool _isBusy;
+        public bool IsBussy
+        {
+            get { return _isBusy; }
+            set => SetProperty(ref _isBusy, value);
+        }
+    }
+}
---
+++ MvvmGuia/VistaModelo/VMPatron.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MvvmGuia.VistaModelo
+{
+    public class VMPatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+        public VMPatron(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+        #region OBJETOS
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+
+        public void ProcesoSincrono()
+        {
+
+        }
+        #endregion
+        #region COMANDO
+        public ICommand ProcesoAsyncCommand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand ProcesoSincronoCommand => new Command(() => ProcesoSincrono());
+        #endregion
+    }
+}
